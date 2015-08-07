379d43d281ce1ab445c54fabe0630931225a8756
Edwin Allen <edwin.allen@xoom.com>
2015-08-07 07:44:57 -0600

    more simpler module using filter function

--- filtered-ls-module/module.js
+++ filtered-ls-module/module.js
@@ -4,17 +4,15 @@ var Path = require('path');
 module.exports = function(path, extension, callback){
  var filesArray = new Array();
  var count = 0;
+
  fs.readdir(path, function(error,files){
    if(error)
     callback(error);
    else{
-    files.forEach(function(file){
-     if(Path.extname(file) === '.'+ extension){
-      filesArray[count]=file;
-      count++;
-     }
-    });
-    callback(null,filesArray);
+     files = files.filter(function(file){
+        return Path.extname(file) === '.' + extension;
+     });
+   callback(null,files);
    }
   }); 
 };
