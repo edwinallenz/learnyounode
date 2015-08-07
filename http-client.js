b3673a0bf5a9478fb71a5f2d83c21a799fd085ac
Edwin Allen <edwin.allen@xoom.com>
2015-08-07 14:07:54 -0600

    Add http client oficial solution

--- http-client.js
+++ http-client.js
@@ -1,10 +1,8 @@
 http = require('http');
-var url = process.argv[2];
 
-http.get(url,function (response){
-  var content = ''; 
+http.get(process.argv[2],function (response){
+  
   response.setEncoding('utf8');
-  response.on("data",function (data){
-    console.log(data); 
-  });
+  response.on('data',console.log);
+  response.on('error',console.error);
 });
