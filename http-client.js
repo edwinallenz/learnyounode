74d885f09a8beabcef31454f329766861dc8a2f1
Edwin Allen <edwin.allen@xoom.com>
2015-08-07 13:26:20 -0600

    Add http client exercise

---
+++ http-client.js
@@ -0,0 +1,10 @@
+http = require('http');
+var url = process.argv[2];
+
+http.get(url,function (response){
+  var content = ''; 
+  response.setEncoding('utf8');
+  response.on("data",function (data){
+    console.log(data); 
+  });
+});
