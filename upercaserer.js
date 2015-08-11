dac60d167588baebf16a293d0510925d6e981cc5
Edwin Allen <edwin.allen@gmail.com>
2015-08-11 13:56:58 -0600

    upercaser with streams

---
+++ upercaserer.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var map = require('through2-map');
+
+var server = http.createServer(function (request, response){
+  response.writeHead(200, { 'content-type': 'text/plain' })
+  request.pipe(map(function(chunk){
+    return chunk.toString().toUpperCase(); 
+  })).pipe(response);
+});
+server.listen(Number(process.argv[2]));
